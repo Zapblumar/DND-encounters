dab106331818275e2e0a6ed17e7649bdeaf840f0
Tylor <Zapblumar@gmail.com>
2021-03-07 12:41:13 -0700

    added some files

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/DND-encounters",
+  {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+module.exports = mongoose.connection;
